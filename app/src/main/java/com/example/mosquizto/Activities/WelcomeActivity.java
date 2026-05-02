95ddde942c1378d7fc19c47c2ac08a647669b61c
DatOnTheRoad <tandatthtt@gmail.com>
2026-05-02 15:28:13 +0700

    feat(auth): integrate /user/profile for persistent login

--- app/src/main/java/com/example/mosquizto/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/mosquizto/Activities/WelcomeActivity.java
@@ -2,15 +2,20 @@ package com.example.mosquizto.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mosquizto.MainActivity;
 import com.example.mosquizto.R;
 import com.example.mosquizto.ViewModels.WelcomeViewModel;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class WelcomeActivity extends AppCompatActivity {
 
     private WelcomeViewModel viewModel;
@@ -22,14 +27,24 @@ public class WelcomeActivity extends AppCompatActivity {
 
         viewModel = new ViewModelProvider(this).get(WelcomeViewModel.class);
 
-        Button btnGoogle      = findViewById(R.id.btnGoogle);
+        //Button btnGoogle      = findViewById(R.id.btnGoogle);
         Button btnSignUpEmail = findViewById(R.id.btnSignUpEmail);
         TextView tvLogin      = findViewById(R.id.tvLogin);
+        Button btnContinue = findViewById(R.id.btnContinue);
 
-        btnGoogle.setOnClickListener(v -> viewModel.onGoogleClicked());
+      //  btnGoogle.setOnClickListener(v -> viewModel.onGoogleClicked());
         btnSignUpEmail.setOnClickListener(v -> viewModel.onEmailClicked());
         tvLogin.setOnClickListener(v -> viewModel.onLoginClicked());
+        btnContinue.setOnClickListener(v -> viewModel.onWelcomeBackClicked());
 
+        viewModel.AfterCreateView();
+        viewModel.UserName.observe(this, name ->
+        {
+            if (name == null)
+            {
+                btnContinue.setVisibility(View.GONE);
+            }else btnContinue.setText("Welcome back " + name);
+        }) ;
         viewModel.navigateTo.observe(this, destination -> {
             if (destination == null) return;
 
@@ -41,6 +56,10 @@ public class WelcomeActivity extends AppCompatActivity {
                     break;
                 case "login":
                     intent = new Intent(this, Login.class);
+                    startActivity(intent);
+                    break;
+                case "main":
+                    intent = new Intent(this, MainActivity.class);
                     startActivity(intent);
                     break;
             }
--- app/src/main/java/com/example/mosquizto/ViewModels/WelcomeViewModel.java
+++ app/src/main/java/com/example/mosquizto/ViewModels/WelcomeViewModel.java
@@ -1,24 +1,45 @@
 package com.example.mosquizto.ViewModels;
 
+import android.se.omapi.Session;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.mosquizto.Dto.response.ApiResponse;
+import com.example.mosquizto.Dto.response.UserResponse;
+import com.example.mosquizto.Network.itf.UserApi;
+import com.example.mosquizto.Services.SessionManager;
+
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 @HiltViewModel
 public class WelcomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> _navigateTo = new MutableLiveData<>();
     public LiveData<String> navigateTo = _navigateTo;
+
+    private final MutableLiveData<String> userName = new MutableLiveData<>() ;
+
+    private final MutableLiveData<String> _errorMessage = new MutableLiveData<>();
+    public LiveData<String> errorMessage = _errorMessage;
+    public LiveData<String> UserName = userName ;
+    private final SessionManager sessionManager ;
+    private final UserApi userApi ;
     @Inject
-    public WelcomeViewModel() {
+    public WelcomeViewModel(SessionManager sessionManager, UserApi userApi)  {
+        this.sessionManager = sessionManager;
+        this.userApi = userApi;
     }
-    public void onGoogleClicked() {
-        _navigateTo.setValue("register");
-    }
+//    public void onGoogleClicked() {
+//        _navigateTo.setValue("register");
+//    }
 
     public void onEmailClicked() {
         _navigateTo.setValue("register");
@@ -33,6 +54,33 @@ public class WelcomeViewModel extends ViewModel {
     }
     public void onWelcomeBackClicked()
     {
+        userApi.getMyProfile().enqueue(new Callback<ApiResponse<UserResponse>>() {
+            @Override
+            public void onResponse(Call<ApiResponse<UserResponse>> call, Response<ApiResponse<UserResponse>> response) {
+                if (response != null && response.body() != null)
+                {
+                    _navigateTo.setValue("main");
+                }else
+                {
+                    //Ko có user profile do token hết hạn
+                    _errorMessage.postValue("You are not logged in");
+                    sessionManager.logout();
+                    userName.postValue("");
+                    Log.e("DEBUG_WELCOME", "API Error: " + response.code());
+                }
+            }
 
+            @Override
+            public void onFailure(Call<ApiResponse<UserResponse>> call, Throwable t) {
+                Log.e("DEBUG_WELCOME", "API Error: " + t.getMessage());
+            }
+        }) ;
+    }
+    public void AfterCreateView()
+    {
+        if(sessionManager != null && sessionManager.getCurrUser() != null)
+        {
+            userName.postValue(sessionManager.getCurrUser().getUsername());
+        }
     }
 }
